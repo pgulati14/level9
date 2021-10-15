d3405d2500f931b7a28718d8ed965ffe940c0016
payal gulati <gulatipayal@yahoo.co.in>
2021-10-16 01:02:11 +0530

    corrected files i somehow lost

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,5 +3,37 @@ class Todo < ActiveRecord::Base
     is_completed = completed ? "[X]" : "[ ]"
     "#{id}. #{due_date.to_s(:short)} #{todo_text} #{is_completed} "
   end
+  def due_today?
+    due_date == Date.today
+  end
 
+  def self.overdue
+    all.where("due_date < ? and completed= ?", Date.today, false)
+  end
+  def self.completed
+    all.where(completed: true)
+  end
+  def self.due_today
+    all.where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-List\n\n"
+    puts "Overdue\n"
+    puts overdue.show_list
+    puts "\n\n"
+    puts "Due Today\n"
+    puts due_date.show_list
+    puts "\n\n"
+    puts "Overdue\n"
+    puts due_later.show_list
+    puts "\n\n"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  #get "todos", to: "todos#index"
-  #post "users/login" , to: "users#login"
-  get "users", to: "users#index"
-  post "users/create", to: "users#create"
-  get "users/:id", to: "users#show"
+  get "/" => "home#index"
+  resources :users
   post "users/login", to: "users#login"
   resources :todos
-  resources :users
-  
+  #get "users", to: "users#index"
+  #post "users/create", to: "users#create"
+  #get "users/:id", to: "users#show"
+  #post "users/login", to: "users#login"
+  #resources :todos
+  #resources :users
 end
