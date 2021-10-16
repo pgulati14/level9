4460487326b6cb139ff693951f56d498dec2f0d3
payal gulati <gulatipayal@yahoo.co.in>
2021-10-16 15:26:54 +0530

    check strike

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -9,9 +9,9 @@ class Todo < ActiveRecord::Base
   def due_today?
     due_date == Date.today
   end
-  def due_today?
-    due_date == Date.today
-  end
+  #def due_today?
+   # due_date == Date.today
+  #end
 
   def self.overdue
     all.where("due_date < ? and completed= ?", Date.today, false)
@@ -23,22 +23,22 @@ class Todo < ActiveRecord::Base
     all.where("due_date = ?", Date.today)
   end
 
-  def self.overdue
-    all.where("due_date < ? and completed= ?", Date.today, false)
-  end
-  def self.completed
-    all.where(completed: true)
-  end
-  def self.due_today
-    all.where("due_date = ?", Date.today)
-  end
+  #def self.overdue
+   # all.where("due_date < ? and completed= ?", Date.today, false)
+  #end
+  #def self.completed
+   # all.where(completed: true)
+  #end
+ # def self.due_today
+  #  all.where("due_date = ?", Date.today)
+  #end
 
   def self.due_later
     all.where("due_date > ?", Date.today)
   end
-  def self.completed
-    all.where(completed: true)
-  end
+#  def self.completed
+ #   all.where(completed: true)
+ # end
 
   def self.show_list
     puts "My Todo-List\n\n"
