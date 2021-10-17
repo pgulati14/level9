a592ca7af6b65dfa9e45a04653358e5a2ace61d8
payal gulati <gulatipayal@yahoo.co.in>
2021-10-17 18:26:47 +0530

    overdue condition test

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -9,13 +9,7 @@ class Todo < ActiveRecord::Base
   def due_today?
     due_date == Date.today
   end
-  #def due_today?
-   # due_date == Date.today
-  #end
 
-  #def self.overdue
-    #all.where("due_date < ? and completed= ?", Date.today, false)
-  #end
   def self.completed
     all.where(completed: true)
   end
@@ -23,24 +17,14 @@ class Todo < ActiveRecord::Base
     all.where("due_date = ?", Date.today)
   end
   def self.overdue
-    where("due_date < ? and (not completed)", Date.today).ordered
+    #where("due_date < ? and (not completed)", Date.today).ordered
+    all.where("due_date < ? and completed= ?", Date.today, false)
   end
-  #def self.overdue
-   # all.where("due_date < ? and completed= ?", Date.today, false)
-  #end
-  #def self.completed
-   # all.where(completed: true)
-  #end
- # def self.due_today
-  #  all.where("due_date = ?", Date.today)
-  #end
 
   def self.due_later
     all.where("due_date > ?", Date.today)
   end
-#  def self.completed
- #   all.where(completed: true)
- # end
+
 
   def self.show_list
     puts "My Todo-List\n\n"
