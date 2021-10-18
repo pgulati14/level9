4a22050df8dca701d76b00e0c74ec6ff72982548
payal gulati <gulatipayal@yahoo.co.in>
2021-10-18 10:50:42 +0530

    checkbox chek

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -17,7 +17,7 @@ class Todo < ActiveRecord::Base
     all.where("due_date = ?", Date.today)
   end
   def self.overdue
-    where("due_date < ? and (not completed)", Date.today)
+    all.where("due_date < ? and (not completed)", Date.today)
     #all.where("due_date < ? and completed= ?", Date.today, false)
   end
 
