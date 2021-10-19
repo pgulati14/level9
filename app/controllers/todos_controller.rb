d73c357b54f8048a145cb3a28fbbc824195ae0be
payal gulati <gulatipayal@yahoo.co.in>
2021-10-19 21:34:10 +0530

    Rufo Formatting done

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,16 +1,15 @@
 # todos_controller.rb
 class TodosController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    #render plain: Todo.order(:due_date).map {|todo| todo.to_pleasant_string}.join("\n")
     render "index"
   end
+
   def show
     id = params[:id]
-    #render plain: " the id you want to see was #{id}"
-    todo =Todo.find(id)
+    todo = Todo.find(id)
     render "todo"
-    #render plain: todo.to_pleasant_string
   end
 
   def create
@@ -18,10 +17,9 @@ class TodosController < ApplicationController
     due_date = DateTime.parse(params[:due_date])
     new_todo = Todo.create!(
       todo_text: todo_text,
-      due_date:  due_date,
+      due_date: due_date,
       completed: false,
-      )
-    #render plain: "this is create action! the params are: #{params.to_s}"
+    )
     response_text = " Hey! your Todo is created with new id #{new_todo.id}"
     render plain: response_text
   end
@@ -29,7 +27,7 @@ class TodosController < ApplicationController
   def update
     id = params[:id]
     completed = params[:completed]
-    todo= Todo.find(id)
+    todo = Todo.find(id)
     todo.completed = completed
     todo.save!
     render plain: "Updated todo completed status to #{completed}"
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,35 +1,36 @@
 # users_controller.rb
 class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    render plain: User.order(:name).map {|user| user.to_pleasant_string}.join("\n")
+    render plain: User.order(:name).map { |user| user.to_pleasant_string }.join("\n")
   end
+
   def create
-    name=params[:name]
-    email=params[:email]
-    password=params[:password]
-    #render plain: "hey! the params are #{params.to_s}"
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
     new_user = User.create!(
       name: name,
       email: email,
       password: password,
     )
-    response_text= "New user created with id #{new_user.id}"
+    response_text = "New user created with id #{new_user.id}"
     render plain: response_text
   end
 
   def show
     id = params[:id]
-    user=User.find(id)
+    user = User.find(id)
     render plain: user.to_pleasant_string
   end
+
   def login
     email = params[:email]
     password = params[:password]
     user = User.find_by(email: email, password: password)
-    if (user == nil) then
-    render plain: "not authentic"
-    else render plain: "authentic"
-    end
+    if (user == nil)
+      render plain: "not authentic"
+    else render plain: "authentic"     end
   end
 end
