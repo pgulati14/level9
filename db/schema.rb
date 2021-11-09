795d630f8f08f78ce505759668ff0fc273a5c605
payal gulati <gulatipayal@yahoo.co.in>
2021-11-09 23:02:53 +0530

    users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_26_145950) do
+ActiveRecord::Schema.define(version: 2021_11_09_060801) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,20 +23,13 @@ ActiveRecord::Schema.define(version: 2021_10_26_145950) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "todos1s", force: :cascade do |t|
-    t.text "todo_text"
-    t.date "due_date"
-    t.boolean "completed"
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
+    t.string "password_digest"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string :first_name
-    t.string :last_name
-    t.string :email
-    t.string :password_digest
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 end
