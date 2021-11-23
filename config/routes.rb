e5c8a94fd233e93262659f2c13612b1ede8fc373
payal gulati <gulatipayal@yahoo.co.in>
2021-11-24 00:33:19 +0530

    resolved BCrypt error

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/" => "home#index"
   resources :todos
   resources :users
+  get "/signin" => "sessions#new", as: :new_sessions
+  post "/signin" => "sessions#create", as: :sessions
 end
