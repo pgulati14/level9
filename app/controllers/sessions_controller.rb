7b9fffe8af71a346907fd1c7f4b2d9beb21d9304
payal gulati <codebind@LAPTOP-L71GSGF9.localdomain>
2021-11-25 23:08:30 +0530

    Validations Done for Email Password textboxes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,8 +9,12 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
       #render plain: "You have entered the correct password !!"
-      session[:current_user_id] = user.id
-      redirect_to "/"
+    elsif !user && !(params[:email].empty?)
+      flash[:error] = " User does not exist!!!"
+      redirect_to new_sessions_path
+    elsif user && !user.authenticate(params[:password])
+      flash[:error] = "password is invalid!!!"
+      redirect_to new_sessions_path
     else
       #render plain: "Incorrect password!!"
       flash[:error] = "Your login attempt was invalid. Please retry."
