9640bfbc025c9d0de02f1c2039b754a13791b6df
payal gulati <codebind@LAPTOP-L71GSGF9.localdomain>
2021-11-27 21:46:36 +0530

    bin folder error

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,10 @@
 class HomeController < ApplicationController
   skip_before_action :ensure_user_logged_in
-
   def index
     if current_user
       redirect_to todos_path
     else
-      render "index", locals: { current_user: current_user }
+      render "index"
     end
   end
 end
